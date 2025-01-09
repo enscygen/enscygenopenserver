11f15d9599345797e00d940b4a4c6f3dc6d2c247
enscygen <enscygen@gmail.com>
2025-01-09 23:35:58 +0530

    Create media.js

---
+++ files/tools/media.js
@@ -0,0 +1,105 @@
+
+            "nutrientAgar": {
+                "name": "Nutrient Agar",
+                "pH": "7",
+                "ingredients": {
+                    "Peptone": { "quantity": 5, "unit": "g" },
+                    "Beef extract": { "quantity": 3.0, "unit": "g" },
+                    "NaCl": { "quantity": 5.0, "unit": "g" },
+                    "Agar": { "quantity": 15.0, "unit": "g" },
+                    "Water": { "quantity": 1000, "unit": "ml" }
+                },
+                "remarks": "Check pH"
+            },
+            "roseBengalAgar": {
+                "name": "Martin's Rose Bengal Agar",
+                "pH": "N/A",
+                "ingredients": {
+                    "Glucose": { "quantity": 10.0, "unit": "g" },
+                    "Peptone": { "quantity": 5.0, "unit": "g" },
+                    "KH2PO4": { "quantity": 1.0, "unit": "g" },
+                    "MgSO4": { "quantity": 0.05, "unit": "g" },
+                    "Streptomycin": { "quantity": 30, "unit": "mg" },
+                    "Agar": { "quantity": 15.0, "unit": "g" },
+                    "Rose Bengal": { "quantity": 35, "unit": "mg" },
+                    "Water": { "quantity": 1000, "unit": "ml" }
+                },
+                "remarks": "Check pH"
+            },
+            "potatoDextroseAgar": {
+                "name": "Potato Dextrose Agar",
+                "pH": "7",
+                "ingredients": {
+                    "Potato": { "quantity": 200, "unit": "g" },
+                    "Dextrose": { "quantity": 20, "unit": "g" },
+                    "Agar": { "quantity": 15, "unit": "g" },
+                    "Water": { "quantity": 1000, "unit": "ml" }
+                },
+                "remarks": "Check pH"
+            },
+            "potatoSucrosePeptoneAgar": {
+                "name": "Potato Sucrose Peptone Agar (PSPA)",
+                "pH": "7",
+                "ingredients": {
+                    "Potato": { "quantity": 200, "unit": "g" },
+                    "Sucrose": { "quantity": 20, "unit": "g" },
+                    "Peptone": { "quantity": 2, "unit": "g" },
+                    "Ca(NO3)2": { "quantity": 0.5, "unit": "g" },
+                    "Na2HPO4": { "quantity": 0.5, "unit": "g" },
+                    "KH2PO4": { "quantity": 0.2, "unit": "g" },
+                    "FeSO4": { "quantity": 0.05, "unit": "g" },
+                    "KCL": { "quantity": 0.05, "unit": "g" },
+                    "Agar": { "quantity": 15, "unit": "g" },
+                    "Water": { "quantity": 1000, "unit": "ml" }
+                },
+                "remarks": "Check pH"
+            },
+            "kenknightAgar": {
+                "name": "Kenknight's Agar Medium",
+                "pH": "N/A",
+                "ingredients": {
+                    "Dextrose": { "quantity": 1, "unit": "g" },
+                    "KH2PO4": { "quantity": 0.1, "unit": "g" },
+                    "NaNO3": { "quantity": 0.1, "unit": "g" },
+                    "KCl": { "quantity": 0.1, "unit": "g" },
+                    "MgSO4": { "quantity": 0.1, "unit": "g" },
+                    "Agar": { "quantity": 15, "unit": "g" },
+                    "Water": { "quantity": 1000, "unit": "ml" }
+                },
+                "remarks": "Check pH"
+            },
+            "ttcAgar": {
+                "name": "TTC/TZC Medium",
+                "pH": "N/A",
+                "ingredients": {
+                    "Glucose": { "quantity": 5, "unit": "g" },
+                    "Peptone": { "quantity": 10, "unit": "g" },
+                    "Casamino acid": { "quantity": 1, "unit": "g" },
+                    "TTC 1%": { "quantity": 5, "unit": "ml" },
+                    "Agar": { "quantity": 15, "unit": "g" },
+                    "Water": { "quantity": 1000, "unit": "ml" }
+                },
+                "remarks": "Check pH"
+            },
+            "oatmealAgar": {
+                "name": "Oatmeal Agar (OA)",
+                "pH": "7",
+                "ingredients": {
+                    "Oatmeal": { "quantity": 30, "unit": "g" },
+                    "Agar": { "quantity": 15, "unit": "g" },
+                    "Water": { "quantity": 1000, "unit": "ml" }
+                },
+                "remarks": "Check pH"
+            },
+            "yeastpotatoAgar": {
+                "name": "Yeast Potato Dextrose Agar (YPDA)",
+                "pH": "7",
+                "ingredients": {
+                    "Yeast Granules": { "quantity": 1, "unit": "g" },
+                    "Potato": { "quantity": 200, "unit": "g" },
+                    "Dextrose": { "quantity": 20, "unit": "g" },
+                    "Agar": { "quantity": 15, "unit": "g" },
+                    "Water": { "quantity": 1000, "unit": "ml" }
+                },
+                "remarks": "Check pH"
+            }
